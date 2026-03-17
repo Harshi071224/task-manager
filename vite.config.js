792c9a68d4937727e8d61bd43fd271fc96462759
Harshitha <harshimustoor@gmail.com>
2026-03-17 14:02:42 +0530

    final base fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/task-manager/',   
+  base: './',   // ✅ MUST be this
   plugins: [react()],
 })
